181fe710ddfe8b490496a2b69ce5f31cd3fa45d6
timefliez1210 <crfabig@gmail.com>
2020-03-25 19:47:07 +0800

    google analytics test

--- pages/blog.js
+++ pages/blog.js
@@ -44,6 +44,16 @@ const Blog = props => (
         href="https://fonts.googleapis.com/css?family=Raleway&display=swap"
         rel="stylesheet"
       ></link>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-160662945-1"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag(){" "}
+        {dataLayer.push(arguments)}
+        gtag("js", new Date()); gtag("config", "UA-160662945-1");
+      </script>
     </Head>
 
     <BlogOverview {...props} />
--- pages/privacy.js
+++ pages/privacy.js
@@ -43,6 +43,16 @@ const Privacy = () => (
         href="https://fonts.googleapis.com/css?family=Raleway&display=swap"
         rel="stylesheet"
       ></link>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-160662945-1"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag(){" "}
+        {dataLayer.push(arguments)}
+        gtag("js", new Date()); gtag("config", "UA-160662945-1");
+      </script>
     </Head>
 
     <section className="services">
