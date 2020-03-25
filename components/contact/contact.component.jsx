2441ceb4ba51003e015a7c95e828e0e339767eba
timefliez1210 <crfabig@gmail.com>
2020-03-25 19:25:40 +0800

    lalala

--- components/contact/contact.component.jsx
+++ components/contact/contact.component.jsx
@@ -34,6 +34,7 @@ const ContactSection = () => (
         <br />
         <br />
         <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label className="form-input-label">
               Name: <input className="form-input" type="text" name="name" />
